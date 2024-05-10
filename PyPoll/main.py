c642d306c521d2bd5ddf9cebf038ce0cae2fa462
Julian-Panditharatne <julianpanditharatne@hotmail.com>
2024-05-10 19:25:51 -0400

    Started PyPoll/main.py

---
+++ PyPoll/main.py
@@ -0,0 +1,10 @@
+import os
+import csv
+
+# Path to collect data from the Resources folder.
+election_data_csv = os.path.join('Resources', 'election_data.csv')
+
+
+
+results = []
+results.append("Election Results\n-------------------------\n")
